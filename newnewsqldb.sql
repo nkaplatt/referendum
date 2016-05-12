0396f81d872331a08ca01368d1e3834c9b5d300f
MarkB3 <MarkB3@users.noreply.github.com>
2016-05-12 22:18:12 +0100

    Update newnewsqldb.sql

--- newnewsqldb.sql
+++ newnewsqldb.sql
@@ -10,25 +10,39 @@ CREATE TABLE `User_tbl` (
  SovandLaw BIT(1),
  Jobs BIT(1),
  DefenceandSecurity BIT(1),
- nochosen INT(8)
+ Nochosen INT(8),
+ Verified BIT(1)
 );
 
 DROP TABLE IF EXISTS `Categories_tbl`;
 CREATE TABLE `Categories_tbl` (
-	Category_ID INT NOT NULL PRIMARY KEY,
-	Name VARCHAR(10),
-	CWeight INT(10),
-	No_Of_SubCards INT(10)
+  Category_ID INT NOT NULL PRIMARY KEY,
+  Name VARCHAR(10),
+  CWeight INT(10),
+  No_Of_SubCards INT(10)
 );
 
 DROP TABLE IF EXISTS `Card_tbl`;
 CREATE TABLE `Card_tbl` (
   Card_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
-	MUser_ID VARCHAR (64),
+  MUser_ID VARCHAR (64),
   Emoticon_Number INT NOT NULL,
   Emoticon_Type INT NOT NULL,
-	Category_ID INT NOT NULL,
-	FOREIGN KEY (Category_ID) REFERENCES Categories_tbl(Category_ID),
-	CAWeight INT(4),
+  Category_ID INT NOT NULL,
+  FOREIGN KEY (Category_ID) REFERENCES Categories_tbl(Category_ID),
+  CAWeight INT(4),
   FOREIGN KEY (MUser_ID) REFERENCES User_tbl(MUser_ID)
 );
+
+DROP TABLE IF EXISTS `Vote_tbl`;
+CREATE TABLE `Vote_tbl` (
+  Vote_ID INT NOT NULL AUTO_INCREMENT PRIMARY KEY,
+  MUser_ID VARCHAR (64),
+  Choice_Number INT NOT NULL, 
+  Choice_Type INT(4), 
+  Category_ID INT NOT NULL,
+  FOREIGN KEY (Category_ID) REFERENCES Categories_tbl(Category_ID),
+  CAWeight INT(4),
+  FOREIGN KEY (MUser_ID) REFERENCES User_tbl(MUser_ID)
+);
+
